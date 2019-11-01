5fe6474907a16291af04e5eed9343df733b0f0dc
Jacual <juancacual@gmail.com>
2019-10-31 22:53:26 -0600

    Version funcional

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,22 +9,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-<<<<<<< HEAD
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/vistas/home.fxml"));
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
         
+        stage.setTitle("Colegio Moria");
         stage.show();
-=======
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/vistas/Imprimir_Cuadro.fxml"));
-        primaryStage.setTitle("Colegio Moriah");
-        primaryStage.setScene(new Scene(root, 759, 750));
-        primaryStage.setResizable(false);
-        primaryStage.show();
->>>>>>> eb163c14368ffa3313f1b26c8af64256e5cfcbd6
     }
 
 
